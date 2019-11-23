54ba4f186986b7e42c628af54fda9e1db438e6cb
BlackCyber009 <57897184+BlackCyber091@users.noreply.github.com>
2019-11-23 20:50:55 +0700

    Add files via upload

---
+++ pulsa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Lumayan 
+#Tools Ini Cuman Buat Iseng� doang
+#Aouthor Mr.Devil
+#Apa liat� mau recode ga guna lu anjing 
+
+echo "\033[0;31m]
+      .---.        .----------
+     /     \  __  /   ------
+    / /     \(째째)/   ----- 
+   //////   * \/ *  ----
+  //// / // :    :----
+ // /   /  /*    *
+//          //..\\          
+===========UU====UU==============
++          *//||\\*              +
++            ****               +
++ coded by : Mr.Devil.          +
++ Team : Dr Cyber Team          +
++ kontak me : 08989956316       +
++               *               +
++         * * *   * * *         +
++       * TransferPulsa *       +
++           **FREE**            +
+================================="
+
+echo "\033[0;32m]Masukan Nomer HP Lu: "
+read lu
+echo "\033[0;32m]Jumblah Pulsa : "
+read pulsa
+echo
+echo
+echo "Tunggu bentar coeg!! Sekitar 20 Detik.."
+echo ""
+sleep 20
+echo "$korban, Sukses Ngirim Pulsa $pulsa"
+echo ""
+sleep 4
+echo "Lu tungguin gabakalan masuk tuh pulsa "
+echo ""
+sleep 4
+echo "Mau pulsa kerja bangsat!! Jangan maunya yg gratisan!!"
